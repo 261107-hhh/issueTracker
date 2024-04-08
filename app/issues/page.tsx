bbf741654910f57167b31e86c55e9fff03505466
Himanshu <himanshunainwal0@gmail.com>
2024-04-08 23:32:32 +0530

    Set up Radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@radix-ui/themes'
 import React from 'react'
 
 const IssuePage = () => {
   return (
-    <div>Issues Page</div>
+    <>
+      {/* <div>Issues Page</div> */}
+      <Button size="3" variant="solid">
+        Edit profile
+      </Button>
+    </>
   )
 }
 
